25b5e5517bc7e0f155721fbd531a035a153162ec
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-03-01 18:05:28 +0100

    fix

--- app/src/main/java/pl/training/runkeeper/ForecastArrayListAdapter.kt
+++ app/src/main/java/pl/training/runkeeper/ForecastArrayListAdapter.kt
@@ -6,14 +6,14 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ForecastArrayListAdapter(private val forecastData: List<String>) : RecyclerView.Adapter<ForecastArrayListAdapter.ViewHolder>() {
 
-    class ViewHolder(val forecastInfo: TextView) : RecyclerView.ViewHolder(forecastInfo)
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(TextView(parent.context))
 
     override fun getItemCount() = forecastData.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       holder.forecastInfo.text = forecastData[position]
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.forecastInfo.text = forecastData[position]
     }
+
+    class ViewHolder(val forecastInfo: TextView) : RecyclerView.ViewHolder(forecastInfo)
 
 }
