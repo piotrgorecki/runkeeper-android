68da5cda5d388ea4833a6c2fec861e2110a1043c
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-03-01 20:39:06 +0100

    fix

--- app/src/main/java/pl/training/runkeeper/forecast/views/ForecastListAdapter.kt
+++ app/src/main/java/pl/training/runkeeper/forecast/views/ForecastListAdapter.kt
@@ -1,9 +1,11 @@
 package pl.training.runkeeper.forecast.views
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import pl.training.runkeeper.R
 import pl.training.runkeeper.commons.formatDate
 import pl.training.runkeeper.commons.formatDegrees
 import pl.training.runkeeper.databinding.ItemForecastListBinding
@@ -12,8 +14,8 @@ import pl.training.runkeeper.forecast.models.DayForecast
 class ForecastListAdapter(private val forecastData: List<DayForecast>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemForecastListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_forecast_list, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount() = forecastData.size
@@ -22,7 +24,9 @@ class ForecastListAdapter(private val forecastData: List<DayForecast>) : Recycle
         viewHolder.bindView(forecastData[position])
     }
 
-    class ViewHolder(private val binding: ItemForecastListBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val binding = ItemForecastListBinding.bind(view)
 
         fun bindView(dayForecast: DayForecast) = with(binding) {
             forecastDescription.text = dayForecast.description
