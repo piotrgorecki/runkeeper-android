969dd2ae14ce0895f8aa2d784321a1c778254e55
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-03-02 10:18:17 +0100

    fix

--- app/src/main/java/pl/training/runkeeper/configuration/ApplicationGraph.kt
+++ app/src/main/java/pl/training/runkeeper/configuration/ApplicationGraph.kt
@@ -3,6 +3,7 @@ package pl.training.runkeeper.configuration
 import dagger.Component
 import pl.training.runkeeper.forecast.ForecastModule
 import pl.training.runkeeper.forecast.viewmodels.ForecastViewModel
+import pl.training.runkeeper.forecast.views.ForecastListFragment
 import javax.inject.Singleton
 
 @Singleton
@@ -10,5 +11,7 @@ import javax.inject.Singleton
 interface ApplicationGraph {
 
     fun inject(forecastViewModel: ForecastViewModel)
+
+    fun inject(forecastListFragment: ForecastListFragment)
 
 }
--- app/src/main/java/pl/training/runkeeper/forecast/views/ForecastListFragment.kt
+++ app/src/main/java/pl/training/runkeeper/forecast/views/ForecastListFragment.kt
@@ -8,16 +8,29 @@ import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jakewharton.rxbinding4.widget.textChanges
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import pl.training.runkeeper.RunKeeperApplication
+import pl.training.runkeeper.RunKeeperApplication.Companion.applicationGraph
+import pl.training.runkeeper.commons.Logger
 import pl.training.runkeeper.databinding.FragmentForecastListBinding
 import pl.training.runkeeper.forecast.viewmodels.ForecastViewModel
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 class ForecastListFragment : Fragment() {
 
+    private val disposableBag = CompositeDisposable()
     private val viewModel: ForecastViewModel by activityViewModels()
     private val forecastListAdapter = ForecastListAdapter()
     private lateinit var binding: FragmentForecastListBinding
+    @Inject
+    lateinit var logger: Logger
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        applicationGraph.inject(this)
         binding = FragmentForecastListBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -34,12 +47,21 @@ class ForecastListFragment : Fragment() {
     }
 
     private fun bindViews() {
-        binding.forecastCityName.doAfterTextChanged {
-            viewModel.getForecast(it.toString())
-        }
+        binding.forecastCityName.textChanges()
+                .map { it.toString() }
+                .filter { it.isNotBlank() }
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(viewModel::getForecast) { logger.log(it.toString()) }
+                .addTo(disposableBag)
         viewModel.forecastData().observe(viewLifecycleOwner) {
             forecastListAdapter.update(it.data)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposableBag.clear()
     }
 
 }
